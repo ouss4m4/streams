9082676b0f0581347fdb69ebec2c96d709c543bd
Oussama <bz.oussama@gmail.com>
2024-12-20 19:48:10 +0400

    typo fix

--- test/downloadImage.test.ts
+++ test/downloadImage.test.ts
@@ -1,3 +1,19 @@
 import { downloadImage } from "../src/lib/downloadImage";
 
-downloadImage("htt.photos/200/300", "./image.png");
+const runTest = async () => {
+  const result = await downloadImage("https://google.com", "./image.png");
+  return result;
+};
+
+(async () => {
+  try {
+    let res = await runTest();
+    if (res instanceof Error) {
+      console.log(res.message);
+    } else {
+      console.log(res);
+    }
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+})();
