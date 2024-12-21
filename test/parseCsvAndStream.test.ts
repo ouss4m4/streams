37def0d90248910774e7e77aa93cf92b5109c3a8
Oussama <bz.oussama@gmail.com>
2024-12-21 13:27:32 +0400

    ts-jest added

---
+++ test/parseCsvAndStream.test.ts
@@ -0,0 +1,37 @@
+import { createReadStream } from "fs";
+import { parse } from "fast-csv";
+import { parseCsvFromDiskAndStream } from "../src/lib/parseCsvFromDiskAndStream";
+
+// Mock fs and fast-csv
+jest.mock("fs", () => ({
+  createReadStream: jest.fn(),
+}));
+
+jest.mock("fast-csv", () => ({
+  parse: jest.fn(),
+}));
+
+describe("parseCsvFromDiskAndStream", () => {
+  it("should parse the CSV file correctly", () => {
+    const mockFilePath = "mocked/path/to/file.csv";
+
+    const mockReadStream = {
+      pipe: jest.fn(),
+    };
+    (createReadStream as jest.Mock).mockReturnValue(mockReadStream);
+
+    const mockParseResult = {
+      sku: "uni1",
+      "image 1": "url 1",
+      "image 2": "url 2",
+      "image 3": "url 3",
+    };
+    (parse as jest.Mock).mockReturnValue(mockParseResult);
+    const result = parseCsvFromDiskAndStream(mockFilePath);
+
+    expect(createReadStream).toHaveBeenCalledWith(mockFilePath);
+    expect(parse).toHaveBeenCalledWith({ headers: true });
+
+    expect(mockReadStream.pipe).toHaveBeenCalledWith(mockParseResult);
+  });
+});
