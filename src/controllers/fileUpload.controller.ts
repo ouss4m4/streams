4d4a7d43e174a64ad4e1f4bffd55a80f2e4caeec
Oussama <oussama@notifyai.io>
2024-12-22 23:21:26 +0400

    change text stream to json

--- src/controllers/fileUpload.controller.ts
+++ src/controllers/fileUpload.controller.ts
@@ -11,57 +11,69 @@ export type IDownloadReponse = {
 
 export class fileUploadController {
   static async handleUpload(req: Request, res: Response): Promise<any> {
-    const startTime = Date.now();
-    logger.info(`started processing  ${req.file?.filename}`);
-
-    const filePath = join(__dirname, "..", "uploads", req.file?.filename ?? "");
-    let csvStream = parseCsvFromDiskAndStream(filePath);
-
-    // Start streaming response
-    res.setHeader("Content-Type", "text/plain");
-    res.write(`Processing started for file: ${req.file?.filename}\n`);
-
-    // File is parsed and stream at this point
-    const finalResult: IDownloadReponse = {
-      success: 0,
-      errors: [],
-    };
-
-    const rowProcessingPromises: Promise<IDownloadReponse>[] = [];
-
-    let rows = 0;
-    // sku, image1, image2, image3
-    for await (const row of csvStream) {
-      rowProcessingPromises.push(fetchAndSaveImagesFromCsvRow(row));
-      if (++rows % 10 == 0) {
-        res.write(`${rows} processed so far \n`);
-      }
+    if (!req.file) {
+      res.status(400).send("No file uploaded.");
+      logger.error("No file uploaded.");
+      return;
     }
-    let rowsProcessingResult = await Promise.allSettled(rowProcessingPromises);
 
-    rowsProcessingResult.forEach((resOrErr) => {
-      if (resOrErr.status == "fulfilled") {
-        finalResult.success += resOrErr.value.success;
-        finalResult.errors = finalResult.errors.concat(resOrErr.value.errors);
+    const startTime = Date.now();
+    const filePath = join(__dirname, "..", "uploads", req.file.filename);
+    logger.info(`Started processing ${req.file.filename}`);
+
+    res.setHeader("Content-Type", "application/json");
+
+    const finalResult: IDownloadReponse = { success: 0, errors: [] };
+    try {
+      let csvStream = parseCsvFromDiskAndStream(filePath);
+
+      // Start streaming response
+      // res.setHeader("Content-Type", "text/plain");
+      // res.write(`Processing started for file: ${req.file?.filename}\n`);
+
+      // // File is parsed and stream at this point
+      // const finalResult: IDownloadReponse = {
+      //   success: 0,
+      //   errors: [],
+      // };
+
+      // const rowProcessingPromises: Promise<IDownloadReponse>[] = [];
+
+      // let rows = 0;
+
+      for await (const row of csvStream) {
+        const rowResult = await fetchAndSaveImagesFromCsvRow(row);
+
+        finalResult.success += rowResult.success;
+        finalResult.errors.push(...rowResult.errors);
+
+        res.write(
+          JSON.stringify({
+            success: true,
+            message: `Processed ${finalResult.success} images successfully.`,
+            errors: finalResult.errors,
+          })
+        );
       }
-    });
+      // let rowsProcessingResult = await Promise.allSettled(
+      //   rowProcessingPromises
+      // );
 
-    logger.info(
-      `Csv parsing finished in ${
-        (Date.now() - startTime) / 1000
-      } seconds for file ${req.file?.filename}`
-    );
-    res.end(
-      JSON.stringify({
-        success: true,
-        message: `Processed ${finalResult.success} images successfully`,
-        errors: finalResult.errors,
-      })
-    );
-    // return res.json({
-    //   success: true,
-    //   message: `Processed ${finalResult.success} images successfully`,
-    //   errors: finalResult.errors,
-    // });
+      // rowsProcessingResult.forEach((resOrErr) => {
+      //   if (resOrErr.status == "fulfilled") {
+      //     finalResult.success += resOrErr.value.success;
+      //     finalResult.errors = finalResult.errors.concat(resOrErr.value.errors);
+      //   }
+      // });
+    } catch (err) {
+      logger.error(`Error processing CSV file: ${(err as Error).message}`);
+    } finally {
+      res.end();
+      logger.info(
+        `Processing finished in ${(Date.now() - startTime) / 1000}s for file ${
+          req.file.filename
+        }`
+      );
+    }
   }
 }
