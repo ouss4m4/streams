be2448bf457eef69b4ea9db1db456fb18f693419
Oussama <bz.oussama@gmail.com>
2024-12-22 23:21:26 +0400

    change text stream to json

--- src/lib/downloadImage.ts
+++ src/lib/downloadImage.ts
@@ -1,25 +1,37 @@
-import { pipeline } from "stream/promises";
 import { createWriteStream } from "fs";
+import { mkdir } from "fs/promises";
+import { dirname } from "path";
+import { pipeline } from "stream/promises";
 
 export const downloadImage = async (
   url: string,
   filePath: string
 ): Promise<boolean | Error> => {
+  const controller = new AbortController();
+  const timeout = setTimeout(() => controller.abort(), 10000); // 10 seconds timeout on image download
+
   try {
-    const response = await fetch(url);
+    await mkdir(dirname(filePath), { recursive: true });
+
+    const response = await fetch(url, { signal: controller.signal });
 
     if (!response.ok || !response.body) {
-      throw new Error(`failed to fetch, status: ${response.status}`);
+      throw new Error(`Failed to fetch, status: ${response.status}`);
     }
-    const writer = createWriteStream(filePath);
 
+    const writer = createWriteStream(filePath);
     await pipeline(response.body as unknown as NodeJS.ReadableStream, writer);
+
     return true;
   } catch (error: any) {
     let message = "Error while downloading image";
-    if (error instanceof Error) {
+    if (error.name === "AbortError") {
+      message = `Request timed out while downloading image`;
+    } else if (error instanceof Error) {
       message = error.message;
     }
-    return new Error(`${message} | ${url} `);
+    return new Error(`${message} | ${url}`);
+  } finally {
+    clearTimeout(timeout);
   }
 };
